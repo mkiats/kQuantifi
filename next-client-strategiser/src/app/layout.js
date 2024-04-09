bd1d2670259089b6fc2d2abe97d3fd6346bcd781
mkiats <mengkiatzz@gmail.com>
2024-04-09 15:23:38 +0800

    SuppressHydrationWarning added for extra style warning

--- next-client-strategiser/src/app/layout.js
+++ next-client-strategiser/src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang='en'>
+		<html lang='en' suppressHydrationWarning={true}>
 			<body
 				className={`${inter.className} bg-background text-foreground`}
 			>
