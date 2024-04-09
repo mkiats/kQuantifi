b3045e913ca308e123ceb5ac03912fcf277c781a
mkiats <mengkiatzz@gmail.com>
2024-04-09 10:50:38 +0800

    Dark mode added

--- next-client-strategiser/src/app/layout.js
+++ next-client-strategiser/src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import React from 'react';
 import './globals.css';
 import Navbar from '@/components/navbar';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,16 +14,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body className={`${inter.className} bg-color60 text-color30`}>
-				<container>
-					<header className='w-screen h-24 bg-transparent flex justify-center items-center gap-4 border-b-2 border-b-color30'>
+			<body
+				className={`${inter.className} bg-background text-foreground`}
+			>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='dark'
+					enableSystem
+					disableTransitionOnChange
+				>
+					<header className='w-screen h-24 bg-transparent flex justify-center items-center gap-4'>
 						<Navbar />
 					</header>
-					<section className='w-screen h-[calc(100vh-6rem) bg-transparent overflow-y-auto'>
+					<section className='w-screen h-[calc(100vh-6rem) overflow-y-auto'>
 						{children}
 					</section>
 					<footer></footer>
-				</container>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
