f7610ff4ba234e8856a9abe4f378de0566f40e75
mkiats <mengkiatzz@gmail.com>
2024-04-11 14:31:33 +0800

    mockData

--- next-client-strategiser/src/api/mockData.jsx
+++ next-client-strategiser/src/api/mockData.jsx
@@ -1,8 +1,13 @@
+import { randomInt } from 'crypto';
+
 const mockStockData = [];
 
 for (let i = 0; i < 1000; i++) {
 	const start = 1642425322;
-	const theObject = { value: i * 5, time: start + i * 86400 };
+	const theObject = {
+		value: (i * 5 + Math.random() * 100) * Math.random(),
+		time: start + i * 86400,
+	};
 	mockStockData.push(theObject);
 }
 
