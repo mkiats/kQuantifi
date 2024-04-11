36766e9fb2c3e0bd35dc07eb91df70bc32c7e99b
mkiats <mengkiatzz@gmail.com>
2024-04-11 15:06:41 +0800

    Springboot files added

---
+++ springboot-server-strategiser/src/main/java/com/mkiats/strategiser/StrategiserApplication.java
@@ -0,0 +1,13 @@
+package com.mkiats.strategiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategiserApplication.class, args);
+	}
+
+}
---
+++ springboot-server-strategiser/src/test/java/com/mkiats/strategiser/StrategiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkiats.strategiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
