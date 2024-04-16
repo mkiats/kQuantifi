faa1a882f0a8a34b6316a028e501924b92dd165b
mkiats <mengkiatzz@gmail.com>
2024-04-16 22:54:52 +0800

    Edited loadingDb script

--- postgres-db-strategiser/scripts/loadTempDb.sql
+++ postgres-db-strategiser/scripts/loadTempDb.sql
@@ -1,14 +1,14 @@
 -- Create table
-CREATE TABLE contacts (
+CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(255),
     email VARCHAR(255),
     password VARCHAR(255),
-    photoUrl VARCHAR(255)
+    photo_url VARCHAR(255)
 );
 
 -- Populate table
-INSERT INTO contacts (username, email, password, photoUrl)
+INSERT INTO users (username, email, password, photo_url)
 VALUES 
     ('user1', 'user1@example.com', 'password1', 'http://example.com/user1.jpg'),
     ('user2', 'user2@example.com', 'password2', 'http://example.com/user2.jpg'),
