ac3d74f080f18dd1f6dd994f7518af39f15496f3
mkiats <mengkiatzz@gmail.com>
2024-04-19 17:11:51 +0800

    Added API to fetch data from alphaVantage

--- spring-server-strategiser/src/main/java/com/mkiats/Application.java
+++ spring-server-strategiser/src/main/java/com/mkiats/Application.java
@@ -1,12 +1,24 @@
 package com.mkiats;
 
+import com.mkiats.service.RetrievalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	private RetrievalService retrievalService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		String tickerData = retrievalService.fetchTickerData("TIME_SERIES_MONTHLY_ADJUSTED", "SPY");
+		System.out.println("TickerData: " + tickerData);
 	}
 }
---
+++ spring-server-strategiser/src/main/java/com/mkiats/service/RetrievalService.java
@@ -0,0 +1,50 @@
+package com.mkiats.service;
+
+import lombok.RequiredArgsConstructor;
+    import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Service
+public class RetrievalService {
+
+    @Value("${api.url.alphavantage}")
+    private String baseApiUrl;
+    @Value("${api.key.alphavantage}")
+    private String apiKey;
+
+    // Dependency injected
+    private final RestTemplate restTemplate;
+
+    public String fetchTickerData(String theTimeframe, String theSymbol) {
+
+        String requestTimeframe;
+        String requestTickerSymbol;
+
+        // Handles API requests
+        if (theTimeframe != null) {
+            requestTimeframe = "TIME_SERIES_MONTHLY_ADJUSTED";
+        } else {
+            requestTimeframe = "TIME_SERIES_WEEKLY_ADJUSTED";
+        }
+        requestTickerSymbol = Objects.requireNonNullElse(theSymbol, "SPY");
+
+
+        String finalisedApiUrl = String.format("%sfunction=%s&symbol=%s&apikey=%s",baseApiUrl,requestTimeframe, requestTickerSymbol,apiKey);
+
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(finalisedApiUrl, String.class);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            String responseBody = responseEntity.getBody();
+            System.out.println(responseBody);
+            return responseBody;
+        } else {
+            return "Error: " + responseEntity.getStatusCode();
+        }
+    }
+
+}
