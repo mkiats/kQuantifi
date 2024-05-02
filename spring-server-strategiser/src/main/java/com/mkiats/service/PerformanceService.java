7e3b28abe46b951be2b7939200a3c79bc3e107b9
mkiats <mengkiatzz@gmail.com>
2024-05-02 17:21:34 +0800

    save work

--- spring-server-strategiser/src/main/java/com/mkiats/service/PerformanceService.java
+++ spring-server-strategiser/src/main/java/com/mkiats/service/PerformanceService.java
@@ -22,27 +22,32 @@ public class PerformanceService {
 	private final RetrievalService retrievalService;
 
 	public void doExecute() throws JsonProcessingException {
+
+//		Retrieve & deserialize json api data
+//		String tickerData = retrievalService.fetchTickerData("TIME_SERIES_MONTHLY_ADJUSTED", "SPY");
 		TempClass json = new TempClass();
 		String jsonStr = json.getJsonStringShort();
-
-		//        COMMENT OUT COZ API REQUEST GOT LIMIT
-		//		String tickerData = retrievalService.fetchTickerData("TIME_SERIES_MONTHLY_ADJUSTED", "SPY");
 		TimeSeriesStockData timeSeriesStockData =
 			retrievalService.convertStringToTimeSeriesStockData(jsonStr);
 
-		InvestmentStrategy newService = investmentStrategyManager.getService(
-			"DollarCostAverageStrategy"
-		);
+//		 Convert FormData -> HashMap
 		HashMap<String, Object> hashmapParameter = new HashMap<>();
 		hashmapParameter.put("timeSeriesStockData", timeSeriesStockData);
 		hashmapParameter.put("periodicAmount", 100);
 		System.out.println(hashmapParameter);
+		
+//		 Retrieve & execute investment service
+		InvestmentStrategy newService = investmentStrategyManager.getService(
+			"DollarCostAverageStrategy"
+		);
 		InvestmentOutput testOutput = newService.executeStrategy(
 			hashmapParameter
 		);
 		testOutput.getStockValue().forEach(element -> {
 			System.out.println(element);
 		});
+
+		// Retrieve & execute financial ratio service
 		FinancialRatioStrategy newFinancialRatio = financialRatioStrategyManager.getService("CompoundAnnualGrowthRate");
 		double cagr = newFinancialRatio.computeRatio(testOutput);
 		System.out.println(cagr);
