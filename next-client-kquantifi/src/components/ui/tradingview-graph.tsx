152392901e39730ebaa24565805449179a639a80
mkiats <mengkiatzz@gmail.com>
2024-05-21 16:23:43 +0800

    TradingViewGraph component done

--- next-client-kquantifi/src/components/ui/tradingview-graph.tsx
+++ next-client-kquantifi/src/components/ui/tradingview-graph.tsx
@@ -2,9 +2,7 @@
 import { useEffect, useRef } from 'react';
 import { AreaData, ColorType, createChart, LineData } from 'lightweight-charts';
 import { hslToRgbString } from '@/lib/utils/utils';
-import {
-	TimeValue,
-} from '@/lib/types/backtest/timeValueSeries';
+import { TimeValue } from '@/lib/types/backtest/timeValueSeries';
 
 interface TradingViewGraphProps {
 	rightOffset: number;
@@ -51,6 +49,7 @@ const TradingViewGraph: React.FC<TradingViewGraphProps> = ({
 				},
 				vertLine: {
 					labelVisible: false,
+					visible: false,
 				},
 			},
 			grid: {
@@ -73,7 +72,7 @@ const TradingViewGraph: React.FC<TradingViewGraphProps> = ({
 				axisPressedMouseMove: false,
 			},
 			timeScale: {
-				rightOffset: 5,
+				rightOffset: rightOffset,
 			},
 		});
 		chart.timeScale().fitContent();
@@ -83,29 +82,121 @@ const TradingViewGraph: React.FC<TradingViewGraphProps> = ({
 			bottomColor: 'rgba(41, 98, 255, 0.28)',
 			lineColor: '#2962FF',
 			lineWidth: 2,
-			crosshairMarkerVisible: false,
+			crosshairMarkerVisible: true,
 		});
 
-		const seriesData: AreaData[] =
-			displayData.map(
-				(element: TimeValue): AreaData => {
-					return {
-						time: element.time,
-						value: element.value,
-					};
-				},
-			);
+		const seriesData: AreaData[] = displayData.map(
+			(element: TimeValue): AreaData => {
+				return {
+					time: element.time,
+					value: element.value,
+				};
+			},
+		);
 		newSeries.setData(seriesData);
 
 		window.addEventListener('resize', handleResize);
+
+		// Tooltip setup
+		const toolTipWidth = 100;
+		const toolTipHeight = 100;
+		const toolTipMargin = 15;
+
+		const toolTip = document.createElement('div');
+
+		toolTip.className = `
+		w-24             /* width: 96px */
+		h-20             /* height: 80px */
+		absolute         /* position: absolute */
+		hidden           /* display: none */
+		p-2              /* padding: 8px */
+		box-border       /* box-sizing: border-box */
+		text-sm          /* font-size: 10px */
+		text-center      /* text-align: center */
+		z-10             /* z-index: 1 */
+		pointer-events-none /* pointer-events: none */
+		border           /* border: 1px solid */
+		rounded-sm       /* border-radius: 2px */
+		bg-[${hslToRgbString(224, 71.4, 4.1)}]  /* background color */
+		text-[${hslToRgbString(210, 20, 100)}]  /* text color */
+		border-[${hslToRgbString(210, 20, 100)}]  /* border color */
+		font-sans        /* font-family: -apple-system, BlinkMacSystemFont, 'Trebuchet MS', Roboto, Ubuntu, sans-serif */
+		antialiased      /* -webkit-font-smoothing: antialiased */
+		subpixel-antialiased /* -moz-osx-font-smoothing: grayscale */`;
+
+		chartContainerRef.current!.appendChild(toolTip);
+
+		// Subscribe to crosshair move event
+		chart.subscribeCrosshairMove((param) => {
+			// console.log(
+			// 	'X: ' + (param.point?.x ?? 0) + 'Y: ' + (param.point?.y ?? 0),
+			// );
+			if (
+				param.point === undefined ||
+				!param.time ||
+				param.point.x < 0 ||
+				param.point.x > chartContainerRef.current!.clientWidth ||
+				param.point.y < 0 ||
+				param.point.y > chartContainerRef.current!.clientHeight
+			) {
+				toolTip.style.display = 'none';
+			} else {
+				const dateStr = param.time;
+				toolTip.style.display = 'block';
+				const data = param.seriesData.get(newSeries);
+				let price;
+
+				if (data) {
+					if ('value' in data) {
+						price = data.value;
+					} else if ('close' in data) {
+						price = data.close;
+					} else {
+						price = 0;
+					}
+				}
+				toolTip.innerHTML = `
+				<div style="color: ${hslToRgbString(210, 20, 100)}">${dateStr}</div>
+				<div style="margin: 4px 0px; color: ${hslToRgbString(210, 20, 100)}">${
+					Math.round(100 * price!) / 100
+				}</div>
+				`;
+				const coordinate = newSeries.priceToCoordinate(price!);
+				let shiftedCoordinate = param.point.x - 50;
+				if (coordinate === null) {
+					return;
+				}
+				shiftedCoordinate = Math.max(
+					0,
+					Math.min(
+						chartContainerRef.current!.clientWidth - toolTipWidth,
+						shiftedCoordinate,
+					),
+				);
+				const coordinateY =
+					coordinate - toolTipHeight - toolTipMargin > 0
+						? coordinate - toolTipHeight - toolTipMargin
+						: Math.max(
+								0,
+								Math.min(
+									chartContainerRef.current!.clientHeight -
+										toolTipHeight -
+										toolTipMargin,
+									coordinate + toolTipMargin,
+								),
+						  );
+				toolTip.style.left = shiftedCoordinate + 'px';
+				toolTip.style.top = coordinateY + 'px';
+			}
+		});
 
 		return () => {
 			window.removeEventListener('resize', handleResize);
 			chart.remove();
 		};
-	}, []);
+	}, [colors, displayData]);
 
-	return <div ref={chartContainerRef} />;
+	return <div ref={chartContainerRef} className='relative' />;
 };
 
 export default TradingViewGraph;
