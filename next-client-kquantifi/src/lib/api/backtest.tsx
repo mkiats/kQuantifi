a4af1f3224ae8df472c46da909146b6d97b678c5
mkiats <mkiats>
2024-09-23 00:06:05 +0800

    edited api

--- next-client-kquantifi/src/lib/api/backtest.tsx
+++ next-client-kquantifi/src/lib/api/backtest.tsx
@@ -1,20 +1,19 @@
-import { BacktestRequest } from "../types/backtest/backtestRequest";
-import { BacktestResponse } from "../types/backtest/backtestResponse";
+import { BacktestRequest } from '../types/backtest/backtestRequest';
+import { BacktestResponse } from '../types/backtest/backtestResponse';
 
-export const getBacktestResult = async (
+export const getBacktestResult = (
 	backtestTicker: BacktestRequest,
 ): Promise<BacktestResponse> => {
-	const response = await fetch('http://localhost:8080/backtests', {
+	return fetch('http://localhost:8080/backtests', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify(backtestTicker),
+	}).then((response) => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		return response.json();
 	});
-	if (!response.ok) {
-		throw new Error('Network response was not ok');
-	}
-	return response.json();
 };
-
-
